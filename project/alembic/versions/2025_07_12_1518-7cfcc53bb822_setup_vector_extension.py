7fa981c4ea1b1c7b45721a2c70f07465bd256d10
leropo00 <leropo00@gmail.com>
2025-07-12 15:41:44 +0200

    added alembic migration tool into project with pgvector database

---
+++ project/alembic/versions/2025_07_12_1518-7cfcc53bb822_setup_vector_extension.py
@@ -0,0 +1,28 @@
+"""setup vector extension
+
+Revision ID: 7cfcc53bb822
+Revises: 
+Create Date: 2025-07-12 15:18:55.007376
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '7cfcc53bb822'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute("CREATE EXTENSION IF NOT EXISTS vector") 
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
