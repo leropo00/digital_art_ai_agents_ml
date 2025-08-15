4052305eb90d5dade8c8399b0106bc157263340e
leropo00 <leropo00@gmail.com>
2025-08-15 15:03:13 +0200

    added cors for frontend access

--- project/app/main.py
+++ project/app/main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .endpoints.art_ideas import router as router_ideas
 
@@ -19,3 +22,16 @@ http://127.0.0.1:8000/docs
 
 app = FastAPI()
 app.include_router(router)
+
+
+origins = [
+    os.getenv("FRONTEND_URL", "http://localhost:3000"),
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
